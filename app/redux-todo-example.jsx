437f12d92abf0fd5e2c9d5231ef47897a98166ef
Joshua Anthony <jantho1990@gmail.com>
2018-05-17 17:31:54 -0500

    subscriptions

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -20,12 +20,26 @@ let reducer = (state = initialState, action) => {
   }
 
 }
-const store = redux.createStore(reducer)
+const store = redux.createStore(
+  reducer,
+  redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ))
+let unsubscribe = store.subscribe(() => {
+  let state = store.getState()
+  document.querySelector('#app').innerHTML = state.searchText
+})
 
-console.log('currentState', store.getState())
 
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'work'
 })
-console.log('currentState', store.getState())
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'hark'
+})
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'faster harder'
+})
